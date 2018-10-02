f25883329f50a35089eec729bea53ceb5b5fc6d8
Nico Vogel <nico-vogel97@outlook.de>
2018-10-02 15:25:59 +0200

    implemented get next

--- node/librarys/GrammerBlock.js
+++ node/librarys/GrammerBlock.js
@@ -3,12 +3,12 @@ const GrammarRule = require('./GrammerRule');
 
 let idCounter = 0;
 
-function getNextID(){
+function getNextID() {
     return ++idCounter;
 }
 
-module.exports = class GrammarBlock{
-    constructor(parts){
+module.exports = class GrammarBlock {
+    constructor(parts) {
         this.parts = parts;
         this.connections = new Map();
         this.id = getNextID();
@@ -16,13 +16,22 @@ module.exports = class GrammarBlock{
 
     getNext() {
         let map = new Map();
-
+        this.parts.forEach(element => {
+            let nextSymbol = element.getSymbolAfterDot();
+            let nextArray = map.get(nextSymbol);
+            if (!nextArray) {
+                nextArray = [];
+                map.set(nextSymbol, nextArray);
+            }
+            nextArray.push(element.getNext());
+        });
+        return map;
     }
 
     contains(parts) {
         parts.forEach(element => {
             this.parts.forEach(part => {
-                if(!part.equals(element)){
+                if (!part.equals(element)) {
                     return false;
                 }
             });
@@ -30,5 +39,5 @@ module.exports = class GrammarBlock{
         return true;
     }
 
-    
+
 }
