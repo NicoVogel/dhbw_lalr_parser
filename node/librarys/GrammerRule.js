0e3d4434a5f96e3626761ad3696d734a6e1136b0
Daniel Bicek <kitt996@gmail.com>
2018-10-02 12:57:54 +0200

    Grammer Rule Complete

--- node/librarys/GrammerRule.js
+++ node/librarys/GrammerRule.js
@@ -1,10 +1,16 @@
 var util = require('./util');
 
+function beginsWithCaptial(name) {
+    return name[0] == name[0].toUpperCase();
+}
+
 module.exports = class GrammerRule {
     constructor(ruleStr) {
         let ruleElems = ruleStr.split(" ");
-        this.leftSide = ruleStr.split(" ")
-        util.assert(ruleStr)
+        this.leftSide = ruleElems[0];
+        util.assert(beginsWithCaptial(this.leftSide), "Left side must be non terminal");
+        util.assert(ruleElems[1] === "->", "GrammerRule must contain arrow");
+        this.rightSide = ruleElems.slice(2);
     }
 
 };
