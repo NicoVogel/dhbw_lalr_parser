285d71a431178e1002588071587154393baebae3
Daniel Bicek <kitt996@gmail.com>
2018-10-02 13:38:10 +0200

    improve grammerblock

--- node/librarys/GrammerBlockPart.js
+++ node/librarys/GrammerBlockPart.js
@@ -4,6 +4,7 @@ class GrammerBlockPart {
     constructor (grammerRule, lookahead) {
         this.grammerRule = grammerRule;
         this.lookahead = lookahead;
+        this.dot_before = 0;
         util.assert(!util.beginsWithCaptial(lookahead), "Lookahead must be terminal")
     }
     
