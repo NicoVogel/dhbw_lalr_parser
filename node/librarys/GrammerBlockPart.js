a4af471559f1b14776dd7003a3bcab489fe2e547
Nico Vogel <nico-vogel97@outlook.de>
2018-10-02 14:44:36 +0200

    add equals method

--- node/librarys/GrammerBlockPart.js
+++ node/librarys/GrammerBlockPart.js
@@ -1,7 +1,7 @@
 var util = require('./util');
 
 module.exports = class GrammerBlockPart {
-    constructor (grammerRule, lookahead) {
+    constructor(grammerRule, lookahead) {
         this.grammerRule = grammerRule;
         this.lookahead = lookahead;
         this.dot_before = 0;
@@ -9,8 +9,14 @@ module.exports = class GrammerBlockPart {
     }
 
     getNext() {
-    
+
     }
 
-    
+    equlas(part) {
+        return !!part
+            && this.dot_before === part.dot_before
+            && this.lookahead === part.lookahead
+            && this.grammerRule.equlas(part.grammerRule);
+    }
+
 }
