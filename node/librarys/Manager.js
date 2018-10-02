7b7f4d4172e61bdaf1af06c2f31a2852adc8ed0d
Daniel Bicek <kitt996@gmail.com>
2018-10-02 15:08:22 +0200

    save

--- node/librarys/Manager.js
+++ node/librarys/Manager.js
@@ -43,11 +43,27 @@ module.exports = class Manager {
         return this.rules.filter((rule) => rule.leftSide === target);
     }
 
+    follows(blockPart) {
+        if (!util.beginsWithCaptial(blockPart.getSymbolAfterDot())) {
+            return blockPart.getSymbolAfterDot();
+        } else {
+            // nextSymbol ist non-terminal. search for follows recursivly
+            // TODO fix this is only working with the first rule, but could be more
+            let rule = this.rulesProducing(blockPart.getSymbolAfterDot())[0];
+            let new_part = new GrammerBlockPart(rule, "shouldnomatter")
+            return this.follows(new_part);
+        }
+    }
+
     expandBlock(block) {
         let hasUnexpaned = true
         block.parts.forEach((part) => {
             if (util.beginsWithCaptial(part.getSymbolAfterDot())) {
-                
+                let new_rules = this.rulesProducing(part.getSymbolAfterDot())
+                let new_parts = new_rules.map((rule) => {
+                    return new GrammerBlockPart(rule,this.follows(part.getNext()))
+                })
+                block.parts.concat(new_parts);
             }
         })
         while (hasUnexpaned) {
