16282055f571cc3d2493639986a904a1e9396a4e
Nico Vogel <nico-vogel97@outlook.de>
2018-10-02 13:54:01 +0200

    addRule method

--- node/librarys/Manager.js
+++ node/librarys/Manager.js
@@ -1,18 +1,34 @@
 const GrammerBlock = require('./GrammerBlock');
 const GrammerRule = require('./GrammerRule');
+const Util = require('./util');
+
+function addAll(set, elements) {
+    elements.forEach(element => {
+        set.add(element);
+    });
+}
 
 
+module.exports = class Manager {
 
-module.exports = class Manager{
-    
     constructor() {
         this.rules = [];
         this.blocks = [];
+        this.terminals = new Set();
+        this.nonTerminals = new Set();
     }
 
     addRule(ruleStr) {
+        const rule = new GrammerRule(ruleStr);
+        addAll(this.terminals, rule.nonTerminals());
+        addAll(this.nonTerminals, rule.terminals());
+        this.rules.push(rule);
+    }
+
+    constructPDA(){
         
     }
+
 
 }
 
